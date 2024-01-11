6c0c63a50143dcfd326145a9ca1e67bf2e9f23b6
tezkerek <andrei.ancuta@gmail.com>
2024-01-11 17:05:49 +0200

    Link assimp when compiling

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 g++ -std=c++20 -Wall -Wextra -g \
-    -lGL -lGLEW -lGLU -lglfw \
+    -lGL -lGLEW -lGLU -lglfw -lassimp \
     -Ilib \
     *.cpp \
     -o scene && ./scene && rm scene
