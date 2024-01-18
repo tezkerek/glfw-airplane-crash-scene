14559de5ff5cb1fda6357c743f5e48ab5cf2e6cf
tezkerek <andrei.ancuta@gmail.com>
2024-01-19 01:46:24 +0200

    Restore default coordinate system

--- scene.cpp
+++ scene.cpp
@@ -1,5 +1,6 @@
 ﻿#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <array>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -13,40 +14,35 @@
 #include "shader.h"
 #include "values.h"
 
-
 const float PI = 3.141592;
 const float windowWidth = 800, windowHeight = 600;
 
 // elemente pentru matricea de vizualizare
-const float Refx = 0.0f, Refy = 0.0f, Refz = 0.0f, znear = 0.1, fov = 45;
+const float Refx = 0.0f, Refy = 0.0f, Refz = 0.0f, znear = 0.1, FOV = 45;
 
 // sursa de lumina
 const float lightX = 500.f, lightY = 100.f, lightZ = 600.f;
 
+constexpr std::array faces = {"assets/skybox/right.jpg",
+                              "assets/skybox/left.jpg",
+                              "assets/skybox/top.jpg",
+                              "assets/skybox/bottom.jpg",
+                              "assets/skybox/front.jpg",
+                              "assets/skybox/back.jpg"};
 
 class Scene {
-    GLuint VaoId, VboId, EboId, skyboxVAO, skyboxVBO, skyboxEboId, myMatrixLocation, matrUmbraLocation,
-        viewLocation, projLocation, lightColorLocation, lightPosLocation,
-        viewPosLocation, codColLocation, grassTextureId;
+    GLuint VaoId, VboId, EboId, skyboxVAO, skyboxVBO, myMatrixLocation,
+        matrUmbraLocation, viewLocation, projLocation, lightColorLocation,
+        lightPosLocation, viewPosLocation, codColLocation, grassTextureId;
 
     float alpha = PI / 8, beta = 0.0f, dist = 400.0f;
-    float Vx = 0.0, Vy = 0.0, Vz = 1.0;
+    float Vx = 0.0, Vy = 1.0, Vz = 0.0;
 
     Shader sceneShader;
     Shader skyboxShader;
     Model airplane;
     Model tree;
-    
-    std::vector<std::string> faces
-    {
-        "assets/skybox/right.jpg",
-        "assets/skybox/left.jpg",
-        "assets/skybox/top.jpg",
-        "assets/skybox/bottom.jpg",
-        "assets/skybox/front.jpg",
-        "assets/skybox/back.jpg"
-    };
-    
+
     unsigned int cubemapTexture = loadCubemap(faces);
 
 public:
@@ -58,67 +54,77 @@ public:
         sceneShader.use();
     }
 
-    void CreateSkyboxVBO(){
+    void CreateSkyboxVBO() {
+        // clang-format off
         GLfloat SkyboxVertices[] = {
-                -1.0f,  1.0f, -1.0f,
-                -1.0f, -1.0f, -1.0f,
-                 1.0f, -1.0f, -1.0f,
-                 1.0f, -1.0f, -1.0f,
-                 1.0f,  1.0f, -1.0f,
-                -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f, -1.0f, -1.0f,
+             1.0f, -1.0f, -1.0f,
+             1.0f, -1.0f, -1.0f,
+             1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
 
-                -1.0f, -1.0f,  1.0f,
-                -1.0f, -1.0f, -1.0f,
-                -1.0f,  1.0f, -1.0f,
-                -1.0f,  1.0f, -1.0f,
-                -1.0f,  1.0f,  1.0f,
-                -1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
 
-                 1.0f, -1.0f, -1.0f,
-                 1.0f, -1.0f,  1.0f,
-                 1.0f,  1.0f,  1.0f,
-                 1.0f,  1.0f,  1.0f,
-                 1.0f,  1.0f, -1.0f,
-                 1.0f, -1.0f, -1.0f,
+             1.0f, -1.0f, -1.0f,
+             1.0f, -1.0f,  1.0f,
+             1.0f,  1.0f,  1.0f,
+             1.0f,  1.0f,  1.0f,
+             1.0f,  1.0f, -1.0f,
+             1.0f, -1.0f, -1.0f,
 
-                -1.0f, -1.0f,  1.0f,
-                -1.0f,  1.0f,  1.0f,
-                 1.0f,  1.0f,  1.0f,
-                 1.0f,  1.0f,  1.0f,
-                 1.0f, -1.0f,  1.0f,
-                -1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+             1.0f,  1.0f,  1.0f,
+             1.0f,  1.0f,  1.0f,
+             1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
 
-                -1.0f,  1.0f, -1.0f,
-                 1.0f,  1.0f, -1.0f,
-                 1.0f,  1.0f,  1.0f,
-                 1.0f,  1.0f,  1.0f,
-                -1.0f,  1.0f,  1.0f,
-                -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+             1.0f,  1.0f, -1.0f,
+             1.0f,  1.0f,  1.0f,
+             1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f, -1.0f,
 
-                -1.0f, -1.0f, -1.0f,
-                -1.0f, -1.0f,  1.0f,
-                 1.0f, -1.0f, -1.0f,
-                 1.0f, -1.0f, -1.0f,
-                -1.0f, -1.0f,  1.0f,
-                 1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+             1.0f, -1.0f, -1.0f,
+             1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+             1.0f, -1.0f,  1.0f,
         };
-        
-            glGenVertexArrays(1, &skyboxVAO);
-            glGenBuffers(1, &skyboxVBO);
-            glBindVertexArray(skyboxVAO);
-            glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(SkyboxVertices), &SkyboxVertices, GL_STATIC_DRAW);
-            glEnableVertexAttribArray(0);
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        // clang-format on
+
+        glGenVertexArrays(1, &skyboxVAO);
+        glGenBuffers(1, &skyboxVBO);
+        glBindVertexArray(skyboxVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+        glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(SkyboxVertices),
+                     &SkyboxVertices,
+                     GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0,
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              3 * sizeof(float),
+                              (void *)0);
     }
-    
+
     void CreateVBO(void) {
         // clang-format off
         GLfloat Vertices[] = {
-            -500.f, -500.f, 0.f, 1.0f,
-            500.f, -500.f, 0.f, 1.0f,
-            500.f, 500.f, 0.f, 1.0f,
-            -500.f, 500.f, 0.f, 1.0f,
+            -500.f, 0.f, -500.f, 1.0f,
+             500.f, 0.f, -500.f, 1.0f,
+             500.f, 0.f,  500.f, 1.0f,
+            -500.f, 0.f,  500.f, 1.0f,
         };
         GLfloat Colors[] = {
             1.f, 1.f, 0.9f,
@@ -152,7 +158,8 @@ public:
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EboId);
 
         glBufferData(GL_ARRAY_BUFFER,
-                     sizeof(Vertices) + sizeof(Colors) + sizeof(Normals) + sizeof(TextureCoordinates),
+                     sizeof(Vertices) + sizeof(Colors) + sizeof(Normals) +
+                         sizeof(TextureCoordinates),
                      NULL,
                      GL_STATIC_DRAW);
         glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertices), Vertices);
@@ -198,13 +205,13 @@ public:
                               (GLvoid *)(sizeof(Vertices) + sizeof(Colors)));
 
         glEnableVertexAttribArray(3); // atributul 3 = texturare
-        glVertexAttribPointer(3,
-                              2,
-                              GL_FLOAT,
-                              GL_FALSE,
-                              2 * sizeof(GLfloat),
-                              (GLvoid *)(sizeof(Vertices) + sizeof(Colors) + sizeof(Normals)));
-
+        glVertexAttribPointer(
+            3,
+            2,
+            GL_FLOAT,
+            GL_FALSE,
+            2 * sizeof(GLfloat),
+            (GLvoid *)(sizeof(Vertices) + sizeof(Colors) + sizeof(Normals)));
     }
 
     void DestroyVBO(void) {
@@ -235,7 +242,8 @@ public:
         lightPosLocation = glGetUniformLocation(programId, "lightPos");
         viewPosLocation = glGetUniformLocation(programId, "viewPos");
         codColLocation = glGetUniformLocation(programId, "codCol");
-        grassTextureId = TextureFromFile("forrest_ground_01.png", "assets/Texture");
+        grassTextureId =
+            TextureFromFile("forrest_ground_01.png", "assets/Texture");
     }
 
     void Draw(void) {
@@ -243,25 +251,22 @@ public:
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
 
-        // pozitia observatorului
+        // Observer position
         float Obsx = Refx + dist * cos(alpha) * cos(beta);
-        float Obsy = Refy + dist * cos(alpha) * sin(beta);
-        float Obsz = Refz + dist * sin(alpha);
+        float Obsy = Refy + dist * sin(alpha);
+        float Obsz = Refz + dist * cos(alpha) * sin(beta);
 
-        // matrice de vizualizare + proiectie
-        glm::vec3 Obs =
-            glm::vec3(Obsx, Obsy, Obsz); // se schimba pozitia observatorului
-        glm::vec3 PctRef =
-            glm::vec3(Refx, Refy, Refz); // pozitia punctului de referinta
-        glm::vec3 Vert =
-            glm::vec3(Vx, Vy, Vz); // verticala din planul de vizualizare
-        glm::mat4 view = glm::lookAt(Obs, PctRef, Vert);
-        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
-        glm::mat4 projection = glm::infinitePerspective(
-            fov,
+        // View and projection
+        glm::vec3 obsVec = glm::vec3(Obsx, Obsy, Obsz);
+        glm::vec3 refVec = glm::vec3(Refx, Refy, Refz);
+        glm::vec3 vertical = glm::vec3(Vx, Vy, Vz);
+        glm::mat4 viewMat = glm::lookAt(obsVec, refVec, vertical);
+        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &viewMat[0][0]);
+        glm::mat4 projectionMat = glm::infinitePerspective(
+            FOV,
             GLfloat(windowWidth) / GLfloat(windowHeight),
             znear);
-        glUniformMatrix4fv(projLocation, 1, GL_FALSE, &projection[0][0]);
+        glUniformMatrix4fv(projLocation, 1, GL_FALSE, &projectionMat[0][0]);
 
         // matricea pentru umbra
         float D = -2.5f;
@@ -306,9 +311,9 @@ public:
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 
         glm::mat4 rotation =
-            glm::rotate(glm::radians(105.0f), glm::vec3(1.0f, 0.25f, 0.0f));
+            glm::rotate(glm::radians(25.0f), glm::vec3(1.0f, 0.0f, 0.5f));
         glm::mat4 scale = glm::scale(glm::vec3(20.0f, 20.0f, 20.0f));
-        glm::mat4 transl = glm::translate(glm::vec3(0.0f, 0.0f, 15.0f));
+        glm::mat4 transl = glm::translate(glm::vec3(0.0f, 15.0f, 0.0f));
         myMatrix = transl * scale * rotation;
         glUniformMatrix4fv(myMatrixLocation, 1, GL_FALSE, &myMatrix[0][0]);
 
@@ -318,34 +323,36 @@ public:
 
         codCol = 2;
         glUniform1i(codColLocation, codCol);
-        rotation = glm::rotate(glm::radians(90.f), glm::vec3(1.0f, 0.f, 0.0f));
         scale = glm::scale(glm::vec3(5.0f, 5.0f, 5.0f));
         for (auto &&[treeType, coordinates] : TREE_COORDINATES) {
             const float treeXCoord = coordinates.first;
-            const float treeYCoord = coordinates.second;
+            const float treeZCoord = coordinates.second;
 
-            transl = glm::translate(glm::vec3(treeXCoord, treeYCoord, 0.0f));
+            transl = glm::translate(glm::vec3(treeXCoord, 0.0f, treeZCoord));
 
-            myMatrix = transl * scale * rotation;
+            myMatrix = transl * scale;
             glUniformMatrix4fv(myMatrixLocation, 1, GL_FALSE, &myMatrix[0][0]);
 
             tree.meshes[treeType].Draw(sceneShader);
         }
-        
-        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+
+        // Change depth function so depth test passes when values are equal to
+        // depth buffer's content
+        glDepthFunc(GL_LEQUAL);
         skyboxShader.use();
-        view = glm::mat4(glm::mat3(view)); // remove translation from the view matrix
-        skyboxShader.setMat4("view", view);
-        skyboxShader.setMat4("projection", projection);
-                // skybox cube
+        // Remove translation from the view matrix
+        viewMat = glm::mat4(glm::mat3(viewMat));
+        skyboxShader.setMat4("view", viewMat);
+        skyboxShader.setMat4("projection", projectionMat);
+        // Skybox cube
         glBindVertexArray(skyboxVAO);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
-        glDepthFunc(GL_LESS); // set depth function back to default
+        // Set depth function back to default
+        glDepthFunc(GL_LESS);
 
-        
         glFlush();
     }
 
@@ -377,36 +384,45 @@ public:
             break;
         }
     }
-    
-    unsigned int loadCubemap(std::vector<std::string> faces)
-    {
-        unsigned int textureID;
+
+    GLuint loadCubemap(const std::array<const char *, 6> &faces) {
+        GLuint textureID;
         glGenTextures(1, &textureID);
         glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
-        int width, height, nrChannels;
         stbi_set_flip_vertically_on_load(false);
-        for (unsigned int i = 0; i < faces.size(); i++)
-        {
-            unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
-            if (data)
-            {
+        for (unsigned int i = 0; i < faces.size(); i++) {
+            int width, height, nrChannels;
+            unsigned char *data =
+                stbi_load(faces[i], &width, &height, &nrChannels, 0);
+            if (data) {
                 glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-                             0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
-                );
+                             0,
+                             GL_RGB,
+                             width,
+                             height,
+                             0,
+                             GL_RGB,
+                             GL_UNSIGNED_BYTE,
+                             data);
                 stbi_image_free(data);
-            }
-            else
-            {
-                std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+            } else {
+                std::cout << "Cubemap tex failed to load at path: " << faces[i]
+                          << std::endl;
                 stbi_image_free(data);
             }
         }
         glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP,
+                        GL_TEXTURE_WRAP_S,
+                        GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP,
+                        GL_TEXTURE_WRAP_T,
+                        GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP,
+                        GL_TEXTURE_WRAP_R,
+                        GL_CLAMP_TO_EDGE);
 
         return textureID;
     }
